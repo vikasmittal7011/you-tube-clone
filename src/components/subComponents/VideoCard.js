2d55547de24ec1cebcf43ef341094809daa69ade
vikas gupta <vikasaggrawal700@gmail.com>
2023-08-05 18:09:09 +0530

    Complete basic of feed component

---
+++ src/components/subComponents/VideoCard.js
@@ -0,0 +1,56 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  demoChannelUrl,
+  demoVideoTitle,
+  demoVideoUrl,
+} from "../../utils/constants";
+import { CheckCircle } from "@mui/icons-material";
+
+const videoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card
+      sx={{
+        width: "358px",
+        borderRadius: 0,
+        boxShadow: "none",
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <CardContent sx={{ background: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoVideoTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default videoCard;
---
+++ src/components/subComponents/Videos.js
@@ -0,0 +1,19 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos?.map((item, i) => (
+        <Box key={i}>
+          {item?.id?.videoId && <VideoCard video={item} />}
+          {item?.id?.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
